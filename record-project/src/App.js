276ad180d9841492b82019a4177a5f08548a0036
yejiahn93 <yejiahn93@gmail.com>
2021-11-10 15:46:05 -0800

    Create App.js

---
+++ record-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+/**import Contents from './components/Contents';**/
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
